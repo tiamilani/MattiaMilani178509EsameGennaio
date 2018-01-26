6c719a1f534aad657a9a46dbe3957a9d2f5f727e
tiamilani <mattia.milani10@gmail.com>
2018-01-26 11:09:39 +0100

    Creata funzione checker, corretto bug package.json

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express'),
     bodyParser = require('body-parser');
 
+const check = require('./checker.js');
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -11,6 +13,14 @@ app.set('port', (process.env.PORT || 5000));
 // a useless function that returns a fixed object. you can use it, if you want, for testing purposes
 app.get('/count',function (req, res) {
     res.json({count: 5})
+})
+
+app.post('/check',function (req, res) {
+    const parameter = request.body;
+    check.check("https://mattiamilani178509esamegennaio.herokuapp.com/count", {}, {count: 3}, 200)
+        .then(result => {
+            res.json(result)
+        });
 })
 
 app.listen(app.get('port'), function() {
